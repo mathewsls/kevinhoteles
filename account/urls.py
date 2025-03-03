c97873425fb8213ecda13f569869e3a6527808fe
Mateo Lopez santibañez <mathewsls1002@gmail.com>
2025-03-03 11:16:42 -0500

    cree eliminar y editar button

--- account/urls.py
+++ account/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('eliminar/<int:id>/', eliminar_reserva, name='eliminar_reserva'),
+    path('editar/<int:id>/', editar_reserva, name='editar_reserva'),
 ]
